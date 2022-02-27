da75fb1e83707222dc91f56c0b3b9c8a47257db3
MystixCode <mystixrage@gmx.ch>
2022-02-27 04:02:39 +0100

    created player scene and world

--- src/server.cpp
+++ src/server.cpp
@@ -4,8 +4,11 @@
 #include <godot_cpp/classes/node.hpp>
 #include <godot_cpp/classes/e_net_multiplayer_peer.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/classes/resource_loader.hpp>
+#include <godot_cpp/classes/resource.hpp>
 #include <player.h>
 #include <string>
+
 
 using namespace godot;
 
@@ -40,25 +43,15 @@ void Server::start_server(int port)
 
 void Server::player_connected(int id) {
     UtilityFunctions::print("player connected: ", id);
+    //TODO load player from scene instead of player.cpp and add player c++ script to it
+    //Resource res = ResourceLoader::get_singleton->load("player.tscn");
+    //Node3D *player = res->instantiate();
+
     CharacterBody3D *player = new Player();
     player->set_name(String::num(id));
     
     NodePath PlayersNodePath = "Players";
     Node *players = get_node_internal(PlayersNodePath);
-   
-    //Todo create a player scene with characterbody3d and meshinstance and collisionshape etc in editor and then instance and add that instead of this player class to Server/Players
-    //Also add cpp script to that characterbody3d somehow
-
-    //outside of this func do:
-    //const Player := preload("res://Player.tscn")
-    //var port := 4242
-
-    //Then convert following gdscript:
-    //var p := Player.instantiate()
-	//p.synced_player_name = "Player " + str(id)
-	//p.position = Vector3(randi_range(-3,3), 5, randi_range(-3,3))
-	//p.name = str(id)
-	//$Players.add_child(p) #Could probably do it via spawn func?
 
     players->add_child(player);
 }
