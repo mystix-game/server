1755f66c2faf0f5fe664caa676b2eb054e93a66f
MystixCode <mystixrage@gmx.ch>
2022-03-05 13:07:04 +0100

    cleanup

--- src/player.cpp
+++ src/player.cpp
@@ -43,16 +43,16 @@ void Player::_bind_methods()
 Player::Player() {
     //UtilityFunctions::print("player constructor");
     
-	//Vector3(randi_range(-3,3), 5, randi_range(-3,3))
+    //Vector3(randi_range(-3,3), 5, randi_range(-3,3))
     set_position(Vector3(0,10,0));
 
-    //TODO
- //   SpringArm3D camera_arm = get_node<SpringArm3D>(NodePath("CameraArm"));
+//TODO
+//    SpringArm3D camera_arm = get_node<SpringArm3D>(NodePath("CameraArm"));
 //    camera = get_node<Camera3D>(NodePath("CameraArm/Camera3D"));
     
     //Camera3D * camera = cast_to<Camera3D>(nodeddd);
     //if multiplayer.multiplayer_peer == null or str(multiplayer.get_unique_id()) == str(name):
-	//camera.current = true
+    //camera.current = true
 }
 
 Player::~Player() {
@@ -65,34 +65,35 @@ void Player::_ready() {
 
 void Player::_physics_process(float delta) {
     //UtilityFunctions::print("Delta: ", String::num(delta));
-
-	Inputs * inputs = get_node<Inputs>(NodePath("Inputs"));
-	synced_position = get_position();
-	Vector3 rotation = get_rotation();
-	synced_rotation_y = rotation.y;
-
-	Vector3 rot = Vector3(inputs->get_mouse_motion().y, inputs->get_mouse_motion().x, 0) * mouse_sensitivity * delta;
+    Inputs * inputs = get_node<Inputs>(NodePath("Inputs"));
+    synced_position = get_position();
+    Vector3 rotation = get_rotation();
+    synced_rotation_y = rotation.y;
+    
+    Vector3 rot = Vector3(inputs->get_mouse_motion().y, inputs->get_mouse_motion().x, 0) * mouse_sensitivity * delta;
     inputs->set_mouse_motion(Vector2());
-	
+
     //Vector3 rot2 = camera_arm->get_rotation();
     //rot2.x = rot2.x - rot.x;
     //camera_arm->set_rotation(rot2);
-	//$CameraArm.rotation.x-= rot.x
-	//synced_camera_arm_rotation_x = $CameraArm.rotation.x
-	//$CameraArm.rotation.x = clamp(rotation.x, -90.0, 30.0) #TODO: limit camera rotation up/down, new Vector3.limit_lengthfunction ?
-	
-	Vector3 tmprot;
-	tmprot.y = rotation.y - rot.y;
-	set_rotation(tmprot);
-	
-	//# Add the gravity.
+
+    //$CameraArm.rotation.x-= rot.x
+    //synced_camera_arm_rotation_x = $CameraArm.rotation.x
+    //$CameraArm.rotation.x = clamp(rotation.x, -90.0, 30.0) #TODO: limit camera rotation up/down, new Vector3.limit_lengthfunction ?
+    
+    Vector3 tmprot;
+    tmprot.y = rotation.y - rot.y;
+    set_rotation(tmprot);
+
+    // Add the gravity.
     if(is_on_floor() == false)
     {
         Vector3 vel = get_motion_velocity();
         vel.y = vel.y - gravity * delta;
         set_motion_velocity(vel);
     }
-    //#Handle Jump.
+
+    //Handle Jump.
     if (inputs->get_jump() == true) {
         Vector3 vel = get_motion_velocity();
         vel.y = jump_force;
@@ -100,26 +101,26 @@ void Player::_physics_process(float delta) {
         synced_mana = synced_mana - 10;
         UtilityFunctions::print("synced_mana: ", synced_mana);
     }
-	//#Handle Shoot
-	//if $Inputs.shoot == true:
-	//	get_node("../../Bullets").spawn([$"Position3D".global_transform.origin, str(name).to_int()])
+    //Handle Shoot
+    //if $Inputs.shoot == true:
+    //    get_node("../../Bullets").spawn([$"Position3D".global_transform.origin, str(name).to_int()])
 
 
-	Basis direction = (get_transform().basis * Vector3(inputs->get_motion().y, 0, inputs->get_motion().x)).orthonormalized();
+    Basis direction = (get_transform().basis * Vector3(inputs->get_motion().y, 0, inputs->get_motion().x)).orthonormalized();
     //TODO
-	//var direction := (transform.basis * Vector3($Inputs.motion.y, 0, $Inputs.motion.x)).normalized()
-	if (direction != Basis()) {
+    //var direction := (transform.basis * Vector3($Inputs.motion.y, 0, $Inputs.motion.x)).normalized()
+    if (direction != Basis()) {
         Vector3 tmp_vel = Vector3(direction.get_rotation_quat().x * speed, 0, direction.get_rotation_quat().z * speed);
-		set_motion_velocity(tmp_vel);
-	}
-	//if direction:
-	//	velocity.x = direction.x * speed
-	//	velocity.z = direction.z * speed
-	//else:
-	//	velocity.x = move_toward(velocity.x, 0, speed)
-	//	velocity.z = move_toward(velocity.z, 0, speed)
+        set_motion_velocity(tmp_vel);
+    }
+    //if direction:
+    //    velocity.x = direction.x * speed
+    //    velocity.z = direction.z * speed
+    //else:
+    //    velocity.x = move_toward(velocity.x, 0, speed)
+    //    velocity.z = move_toward(velocity.z, 0, speed)
 
-	move_and_slide();
+    move_and_slide();
 }
 
 Vector3 Player::get_synced_position() { return synced_position; }
