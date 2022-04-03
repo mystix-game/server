d349053b9997a498a895c2637e548b168d18829a
bitcubix <bitcubix@bitcubix.io>
2022-04-03 14:58:30 +0200

    add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# arguments:
+# 1 (required): the godot executable file with path
+# 2 (optional): the number of thread for the build
+
+GODOT_EXECUTABLE="$1"
+
+if [ -z "$2" ]; then
+  BUILD_J=""
+else
+  BUILD_J="-j$2"
+fi
+
+scons target=debug "$BUILD_J"
+
+$GODOT_EXECUTABLE --debug --headless --path ./server
